47d386236c7445206485a8ce75d56f6c9c087b38
Yu-T0000 <chiave.yu@gmail.com>
2023-03-26 21:14:16 +0900

    修正 27

--- ice_ar/ice_ar.ino
+++ ice_ar/ice_ar.ino
@@ -1,5 +1,6 @@
 const int pin = A0;
-float temp,TMP;
+float temp,TMP,mode;
+int E;
 
 void setup() {
   
@@ -11,16 +12,20 @@ void loop() {
 
   temp = ((analogRead(pin)/1024.0)*5.0-0.5)*100;
   TMP=temp*100;
-
+  mode = analogRead(A2)/127;
+  if(mode>=1){E = 1;}
+  if(mode<0){E = 0;}
+  
 if(Serial.available()==1){  //1バイトのデータを受信
   byte inBuf[1];//配列一個
   Serial.readBytes(inBuf,1);//ポートの文字列一個をinBufに入れる
   if(inBuf[0]=='s'){
-    byte outBuf[4];
+    byte outBuf[5];
     outBuf[0] = 's';
     outBuf[1] = (int16_t)(TMP)>>8;//上位
     outBuf[2] = (int16_t)(TMP)&0xFF;//下位
-    outBuf[3] = 'e';
+    outBuf[3] = E;
+    outBuf[4] = 'e';
     Serial.write(outBuf,4);
     }
     else{
